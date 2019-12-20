ec61a234b0ca459e73519eece977fabe512e708a
Jsen <w2015long@qq.com>
2019-12-20 22:29:13 +0800

    login

--- src/containers/login/login.jsx
+++ src/containers/login/login.jsx
@@ -1,15 +1,62 @@
 import React, {Component} from 'react';
+import {
+    NavBar,
+    WingBlank,
+    WhiteSpace,
+    List,
+    InputItem,
+    Button,
+} from 'antd-mobile'
+import Logo from 'components/logo/logo'
+
+const ListItem = List.Item;
 
 class Login extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            username:'',
+            password:'',
+        }
+    }
+
+    handleChange = (type,val)=> {
+        this.setState({
+            [type]:val
+        })
+    }
+
+    register = () => {
+        console.log(this.state);
     }
 
     render() {
+        const {select} = this.state
         return (
-            <div>
-                Login
+            <div style={{background:'#f1f1f1'}}>
+                <NavBar mode="dark">阿里直聘</NavBar>
+                <Logo />
+                <WingBlank>
+                    <List>
+                        <InputItem
+                            type="text"
+                            placeholder="用户名"
+                            onChange={val=>this.handleChange('username',val)}
+                        >用户名</InputItem>
+                    </List>
+                    <WhiteSpace />
+                    <List>
+                        <InputItem
+                            type="password"
+                            placeholder="密码"
+                            onChange={val=>this.handleChange('password',val)}
+                        >密码</InputItem>
+                    </List>
+                    <WhiteSpace />
+                    <Button type='primary' onClick={this.register}>登&nbsp;陆</Button>
+                    <WhiteSpace />
+                    <Button type='ghost' onClick={()=>this.props.history.replace('/register')}>注&nbsp;册</Button>
+                </WingBlank>
             </div>
         )
     }
